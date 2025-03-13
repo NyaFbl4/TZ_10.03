b69fdf4c4fe9451e826b497e5dab1ca8ea60131a
Измайлов Никита <nikitaizmaylov1402@gmail.com>
2025-03-13 17:54:24 +0300

    Убрал комментарии

--- Assets/Testovoe/Scripts/Joystick/JoystickForMovement.cs
+++ Assets/Testovoe/Scripts/Joystick/JoystickForMovement.cs
@@ -5,24 +5,6 @@ namespace Testovoe
 {
     public class JoystickForMovement : JoystickHandler
     {
-        /*
-        private void Uupdate()
-        {
-            if (_inputVector.x != 0 || _inputVector.y != 0)
-            {
-                _characterMovement.MoveCharacter(
-                    new Vector3(_inputVector.x, 0, _inputVector.y));
-                //_characterMovement.RotateCharacter(new Vector3(_inputVector.x, 0, _inputVector.y));
-            }
-            else
-            {
-                _characterMovement.MoveCharacter(
-                    new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
-                //_characterMovement.RotateCharacter(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
-            }
-        }
-        */
-
         public Vector3 ReturnVectorDirection()
         {
             if (_inputVector.x != 0 || _inputVector.y != 0)
